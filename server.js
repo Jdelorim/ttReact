07854473812874fd1efb55c77b99bb57ad1aae56
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.localdomain>
2019-05-14 15:35:05 -0400

    adding routes

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 const PORT = process.env.PORT || 4000;
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+//   }
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
