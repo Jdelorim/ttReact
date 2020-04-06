fe5ae1b379c019afc248da5341561ae8efadf697
Joshua <joshuadelorimier@gmail.com>
2020-04-06 16:52:27 -0400

    r

---
+++ client/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className='container'>
+                Hi
+            </div>
+        )
+    }
+}
+
+export default App;
