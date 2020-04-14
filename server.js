b29a4f76e26a7469c70b19cbd1b39a0c24af82c8
Joshua <joshuadelorimier@gmail.com>
2020-04-13 21:32:55 -0400

    ssl

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static("client/build"));
 }
 
+//forces ssl 
+const enforce = require('express-sslify');
+ app.use(enforce.HTTPS({ trustProtoHeader: true }))
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 })
